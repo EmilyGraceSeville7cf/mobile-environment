53f33a80a1fa01f70a566a5de9312612feb6be7f
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-09-08 22:04:37 +1000

    feat(script): rewrite script for repository creation

--- utils/inputs.fish
+++ utils/inputs.fish
@@ -1,5 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/env fish
 
+source /data/data/com.termux/files/home/.shortcuts/utils/constants.fish
+
+function get --argument-names input field
+    echo $input | yq --input-format=json .$field
+end
+
 function spinner --argument-names title
     set options $argv[2..]
 
@@ -9,10 +15,17 @@ function spinner --argument-names title
 end
 
 function text --argument-names title
-    termux-dialog text \
-        -t "$title"
-end
+    set info (termux-dialog text \
+                -t "$title")
+    
+    set hint
 
-function get --argument-names input field
-    echo $input | yq --input-format=json .$field
+    if test "$(get info code)" = $success && test -z "$(get info text)"
+        set hint -i "❌ not empty input"
+    end
+    
+    while test "$(get info code)" = $success && test -z "$(get info text)"
+        set info (termux-dialog text \
+            -t "$title")
+    end
 end
